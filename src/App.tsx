dc2d33dae0c52abf81f5d9690dabe4e35f107d0b
snerks <martin.github@outlook.com>
2018-09-30 17:22:45 +0100

    Add toggle

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ class App extends React.Component {
       //     To get started, edit <code>src/App.tsx</code> and save to reload.
       //   </p>
       // </div>
+
       <PointsLineChart />
     );
   }
--- src/components/points-line-chart.tsx
+++ src/components/points-line-chart.tsx
@@ -1,168 +1,174 @@
 import * as React from "react";
 
-import { Line } from "react-chartjs-2";
+import { Line, ChartData } from "react-chartjs-2";
+import * as chartjs from "chart.js";
 
 export interface PointsLineChartProps {}
 
-export interface PointsLineChartState {}
+export interface PointsLineChartState {
+  showTrendLines: boolean;
+}
 
 class PointsLineChart extends React.Component<
   PointsLineChartProps,
   PointsLineChartState
 > {
-  // state = { :  }
-  render() {
-    // const chartData = {
-    //   labels: ["1", "2", "3", "4", "5", "6"],
-    //   datasets: [
-    //     {
-    //       label: "2017-2018",
-    //       data: [3, 3, 4, 5, 6, 9],
-    //       //   backgroundColor: [
-    //       //     "rgba(255, 99, 132, 0.2)",
-    //       //     "rgba(54, 162, 235, 0.2)",
-    //       //     "rgba(255, 206, 86, 0.2)",
-    //       //     "rgba(75, 192, 192, 0.2)",
-    //       //     "rgba(153, 102, 255, 0.2)",
-    //       //     "rgba(255, 159, 64, 0.2)"
-    //       //   ],
-    //       borderColor: [
-    //         "rgba(255,99,132,1)",
-    //         "rgba(54, 162, 235, 1)",
-    //         "rgba(255, 206, 86, 1)",
-    //         "rgba(75, 192, 192, 1)",
-    //         "rgba(153, 102, 255, 1)",
-    //         "rgba(255, 159, 64, 1)"
-    //       ],
-    //       borderWidth: 1
-    //     },
-    //     {
-    //       label: "2018-2019",
-    //       data: [1, 2, 2, 5, 8, 11],
-    //       //   backgroundColor: [
-    //       //     "rgba(255, 99, 132, 0.2)",
-    //       //     "rgba(54, 162, 235, 0.2)",
-    //       //     "rgba(255, 206, 86, 0.2)",
-    //       //     "rgba(75, 192, 192, 0.2)",
-    //       //     "rgba(153, 102, 255, 0.2)",
-    //       //     "rgba(255, 159, 64, 0.2)"
-    //       //   ],
-    //       borderColor: [
-    //         "rgba(255,99,132,1)",
-    //         "rgba(54, 162, 235, 1)",
-    //         "rgba(255, 206, 86, 1)",
-    //         "rgba(75, 192, 192, 1)",
-    //         "rgba(153, 102, 255, 1)",
-    //         "rgba(255, 159, 64, 1)"
-    //       ],
-    //       borderWidth: 1
-    //     }
-    //   ]
-    // };
+  state = { showTrendLines: false };
 
-    const chartData = {
-      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+  handleShowTrendLines = () => {
+    this.setState(prevState => {
+      const nextState = { ...prevState };
+      nextState.showTrendLines = !prevState.showTrendLines;
+
+      return nextState;
+    });
+  };
+
+  render() {
+    const redRgba = "255, 0, 0";
+    const redLineColour = `rgba(${redRgba}, 1)`;
+    const redLegendFillColour = `rgba(${redRgba}, 0.4)`;
+
+    // 128, 0, 128
+    const purpleRgba = "85, 85, 85";
+    const purpleLineColour = `rgba(${purpleRgba}, 1)`;
+    const purpleLegendFillColour = `rgba(${purpleRgba}, 0.4)`;
+
+    const greyRgba = "220, 220, 220";
+    const greyLineColour = `rgba(${greyRgba}, 1)`;
+    // const greyLegendFillColour = `rgba(${greyRgba}, 0.4)`;
+
+    const greenRgba = "75, 192, 192";
+    const greenLineColour = `rgba(${greenRgba}, 1)`;
+    const greenLegendFillColour = `rgba(${greenRgba}, 0.4)`;
+
+    const orangeRgba = "255, 165, 0";
+    const orangeLineColour = `rgba(${orangeRgba}, 1)`;
+    const orangeLegendFillColour = `rgba(${orangeRgba}, 0.4)`;
+
+    // const showTrendLines = true;
+
+    const dataPointCount = 15;
+
+    const chartData: ChartData<chartjs.ChartData> = {
+      // labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      // labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+
+      labels: Array(dataPointCount)
+        .fill(0)
+        .map((e, i) => (i + 1).toString(10)),
+
       datasets: [
         {
           label: "2017-2018",
           fill: false,
           lineTension: 0.1,
 
-          // backgroundColor: "rgba(75,192,192,0.4)",
-          // borderColor: "rgba(75,192,192,1)",
-          backgroundColor: "rgba(255, 0, 0, 0.4)",
-          borderColor: "rgba(255, 0, 0, 1)",
+          backgroundColor: purpleLegendFillColour,
+          borderColor: purpleLineColour,
 
           borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: "miter",
-          pointBorderColor: "rgba(255, 0, 0, 1)",
+          pointBorderColor: greyLineColour,
           pointBackgroundColor: "#fff",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBackgroundColor: purpleLineColour,
+          pointHoverBorderColor: greyLineColour,
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [3, 3, 4, 5, 6, 9, 10, 13, 14, 17]
+          data: [3, 3, 4, 5, 6, 9, 10, 13, 14, 17, 20, 20, 20, 23, 24]
         },
         {
           label: "2018-2019",
           fill: false,
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
+          backgroundColor: redLegendFillColour,
+          borderColor: redLineColour,
           borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
+          pointBorderColor: redLineColour,
           pointBackgroundColor: "#fff",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBackgroundColor: redLineColour,
+          pointHoverBorderColor: greyLineColour,
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
           data: [1, 2, 2, 5, 8, 11, 14, 14, 14, 15]
-        },
-        {
-          label: "Playoff Form",
-          fill: false,
-          lineTension: 0.1,
-
-          // backgroundColor: "rgba(75,192,192,0.4)",
-          // borderColor: "rgba(75,192,192,1)",
-          backgroundColor: "rgba(220,220,220, 0.4)",
-          borderColor: "rgba(220,220,220, 1)",
-
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(220,220,220, 1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(220,220,220, 1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [1.7, 3.4, 5.1, 6.8, 8.5, 10.2, 11.9, 13.6, 15.3, 17]
-        },
-        {
-          label: "Relegation Form",
-          fill: false,
-          lineTension: 0.1,
-
-          // backgroundColor: "rgba(75,192,192,0.4)",
-          // borderColor: "rgba(75,192,192,1)",
-          backgroundColor: "rgba(255, 165, 0, 0.4)",
-          borderColor: "rgba(255, 165, 0, 1)",
-
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(255, 165, 0, 1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(255, 165, 0, 1)",
-          pointHoverBorderColor: "rgba(255, 165, 0,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         }
       ]
     };
 
+    if (this.state.showTrendLines) {
+      if (chartData.datasets) {
+        chartData.datasets.push({
+          label: "Playoff Form",
+          fill: false,
+          lineTension: 0.1,
+
+          backgroundColor: greenLegendFillColour,
+          borderColor: greenLineColour,
+
+          borderCapStyle: "butt",
+          borderDash: [5, 15],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: greenLineColour,
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: greenLineColour,
+          pointHoverBorderColor: greyLineColour,
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+
+          // data: [1.7, 3.4, 5.1, 6.8, 8.5, 10.2, 11.9, 13.6, 15.3, 17]
+          data: Array(dataPointCount)
+            .fill(0)
+            .map((e, i) => (i + 1) * 1.7)
+        });
+
+        chartData.datasets.push({
+          label: "Relegation Form",
+          fill: false,
+          lineTension: 0.1,
+
+          backgroundColor: orangeLegendFillColour,
+          borderColor: orangeLineColour,
+
+          borderCapStyle: "butt",
+          borderDash: [5, 15],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: orangeLineColour,
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: orangeLineColour,
+          pointHoverBorderColor: greyLineColour,
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+
+          // data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+          data: Array(dataPointCount)
+            .fill(0)
+            .map((e, i) => (i + 1) * 1)
+        });
+      }
+    }
+
     const chartOptions = {
+      // responsive: true,
+      maintainAspectRatio: false,
       scales: {
         yAxes: [
           {
@@ -187,7 +193,28 @@ class PointsLineChart extends React.Component<
     };
 
     return (
-      <Line data={chartData} options={chartOptions} width={600} height={250} />
+      <div style={{ position: "relative", height: "80vh" }}>
+        <div
+          className="custom-control custom-checkbox"
+          style={{ margin: "30px" }}
+        >
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="customCheck1"
+            onChange={() => this.handleShowTrendLines()}
+          />
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Show Trend Lines
+          </label>
+        </div>
+        <Line
+          data={chartData}
+          options={chartOptions}
+          width={600}
+          redraw={true}
+        />
+      </div>
     );
   }
 }
